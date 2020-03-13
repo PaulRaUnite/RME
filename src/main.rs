9b1148fd0304c374b3eb0914533f2088cad5aafd
Pavlo Tokariev <pavlo.tokariev@gmail.com>
2020-03-13 15:15:25 +0200

    Improvements.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+extern crate clap;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-extern crate clap;
 
-use clap::Arg;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
+
+use clap::Arg;
 
 mod urm;
 
@@ -20,15 +21,34 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .required(true)
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("values")
+                .index(2)
+                .required(false)
+                .takes_value(true)
+                .multiple(true),
+        )
         .get_matches();
 
     let filename = matches.value_of("FILE").unwrap();
+    let values = matches
+        .values_of("values")
+        .unwrap()
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<u64>>();
 
+    let content = read_file(filename)?;
+
+    println!(
+        "{:?}",
+        urm::Application::from_str(&content)?.run(&values)?
+    );
+    Ok(())
+}
+
+fn read_file(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
     let mut file = File::open(filename)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
-
-    let program = urm::parse(&content)?;
-    println!("{:?}", program);
-    Ok(())
+    Ok(content)
 }
--- src/urm.rs
+++ src/urm.rs
@@ -16,6 +16,7 @@ pub enum Instruction {
     },
 }
 
+#[derive(Debug)]
 pub enum Memory {
     Linear(Vec<u64>),
     Sparse(HashMap<usize, u64>),
@@ -25,26 +26,65 @@ pub struct Program(Vec<Instruction>);
 
 impl Memory {
     pub fn new_linear(size: usize) -> Self {
-        let mut vec = Vec::with_capacity(size);
-        vec.resize(size, 0);
-        Memory::Linear(vec)
+        Memory::Linear(vec![0; size])
     }
 
-    pub fn new_sparse<'a>(active_registers: impl IntoIterator<Item = &'a usize>) -> Self {
-        Memory::Sparse(HashMap::from_iter(
-            active_registers.into_iter().map(|x| (*x, 0)),
-        ))
+    pub fn new_sparse<'a>(active_registers: impl Iterator<Item = &'a usize>) -> Self {
+        Memory::Sparse(HashMap::from_iter(active_registers.map(|x| (*x, 0))))
     }
 
-    pub fn new<'a>(active_registers: impl IntoIterator<Item = &'a usize>) -> Self {
+    pub fn from_program(program: &Program) -> Self {
+        Self::new(program.iter_registers())
+    }
+
+    pub fn new<'a>(active_registers: impl Iterator<Item = &'a usize>) -> Self {
         Self::new_sparse(active_registers)
+    }
+}
+
+#[derive(Debug)]
+pub struct Application {
+    memory: Memory,
+    program: Program,
+}
+
+impl Application {
+    pub fn from_str(content: &str) -> Result<Application, Box<dyn std::error::Error>> {
+        let program = parse(content)?;
+        let memory = Memory::from_program(&program);
+        Ok(Application { memory, program })
+    }
+
+    pub fn run(&mut self, arguments: &[u64]) -> Result<u64, Box<dyn std::error::Error>> {
+        Ok(0)
+    }
+}
+
+impl Index<usize> for Memory {
+    type Output = u64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match self {
+            Memory::Linear(vector) => vector.index(index),
+            Memory::Sparse(map) => map.index(&index),
+        }
+    }
+}
+
+impl IndexMut<usize> for Memory {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match self {
+            Memory::Linear(vector) => vector.index_mut(index),
+            Memory::Sparse(map) => map.get_mut(&index).unwrap(),
+        }
     }
 }
 
 use itertools::{enumerate, Itertools};
 use pest::Parser;
 use std::fmt;
-use std::fmt::{Debug, Error, Formatter};
+use std::fmt::{Debug, Formatter};
+use std::ops::{Index, IndexMut};
 
 #[derive(Parser)]
 #[grammar = "urm.pest"]
@@ -62,40 +102,40 @@ pub fn parse(program: &str) -> Result<Program, Box<dyn std::error::Error>> {
             Rule::jump => {
                 let (first, second, goto) = pair
                     .into_inner()
-                    .map(|x| x.as_str().parse())
+                    .map(|x| x.as_str().parse().unwrap())
                     .next_tuple()
                     .unwrap();
                 Instruction::Jump {
-                    reg1: first?,
-                    reg2: second?,
-                    goto: goto?,
+                    reg1: first,
+                    reg2: second,
+                    goto,
                 }
             }
             Rule::succ => {
                 let register = pair
                     .into_inner()
-                    .map(|x| x.as_str().parse())
+                    .map(|x| x.as_str().parse().unwrap())
                     .next()
-                    .unwrap()?;
+                    .unwrap();
                 Instruction::Increase(register)
             }
             Rule::zero => {
                 let register = pair
                     .into_inner()
-                    .map(|x| x.as_str().parse())
+                    .map(|x| x.as_str().parse().unwrap())
                     .next()
-                    .unwrap()?;
+                    .unwrap();
                 Instruction::Zero(register)
             }
             Rule::tran => {
                 let (reg1, reg2) = pair
                     .into_inner()
-                    .map(|x| x.as_str().parse())
+                    .map(|x| x.as_str().parse().unwrap())
                     .next_tuple()
                     .unwrap();
                 Instruction::Translate {
-                    from: reg1?,
-                    to: reg2?,
+                    from: reg1,
+                    to: reg2,
                 }
             }
             Rule::num => panic!("should not be in the context"),
@@ -115,5 +155,51 @@ impl fmt::Debug for Program {
             }
         }
         Ok(())
+    }
+}
+
+impl Program {
+    pub fn iter_registers(&self) -> impl Iterator<Item = &usize> {
+        self.0.iter().flat_map(|i| i.iter_registers())
+    }
+}
+
+impl Instruction {
+    pub fn iter_registers(&self) -> impl Iterator<Item = &usize> {
+        InstructionRegisters {
+            instruction: self,
+            index: 0,
+        }
+    }
+}
+
+struct InstructionRegisters<'a> {
+    instruction: &'a Instruction,
+    index: u8,
+}
+
+impl<'a> Iterator for InstructionRegisters<'a> {
+    type Item = &'a usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = match self.instruction {
+            Instruction::Increase(register) | Instruction::Zero(register) => match self.index {
+                0 => Some(register),
+                _ => None,
+            },
+            Instruction::Translate {
+                from: reg1,
+                to: reg2,
+            }
+            | Instruction::Jump { reg1, reg2, .. } => match self.index {
+                0 => Some(reg1),
+                1 => Some(reg2),
+                _ => None,
+            },
+        };
+        if result.is_some() {
+            self.index += 1
+        }
+        result
     }
 }
